a05f5be601132301148bcc00db29bd25e626973e
jz-huang <jiangzehuang@gmail.com>
2018-10-13 00:06:44 -0700

    fix port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
 var vizSocket;
 var remoteSocket;
 app.get('/', function(req, res){
@@ -34,6 +35,6 @@ io.on('connection', function(socket){
   })
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(port, function(){
+  console.log('listening on *:');
 });
